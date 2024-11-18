40c63c440464ed6709b28530b190e371d1ed4413
Eddie Woo <archaco123@gmail.com>
2024-11-18 23:05:02 +0900

    ---

---
+++ src/main/java/com/est/view_exp/ViewExpApplication.java
@@ -0,0 +1,13 @@
+package com.est.view_exp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewExpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewExpApplication.class, args);
+    }
+
+}
